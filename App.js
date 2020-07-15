fa4680f24c088cb73f6e448c51c7d36d0c141d1a
David <jobroman83@gmail.com>
2020-07-15 19:00:28 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, View, Text, SafeAreaView} from 'react-native'
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+         <Text> Hola mundo</Text>
+    </SafeAreaView>
+ 
+  )
+}
+
+export default App
