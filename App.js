b91cf12e301f67edf6fa8d5a8766ad97efc02dac
David <jobroman83@gmail.com>
2020-07-15 19:15:43 +0200

    creating structure

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import {StyleSheet, View, Text, SafeAreaView} from 'react-native'
 
 
 const App = () => {
   return (
-    <SafeAreaView>
-         <Text> Hola mundo</Text>
-    </SafeAreaView>
- 
+    <Fragment>
+      {/* <> */}
+          <SafeAreaView style={styles.safeArea}>
+            <Text>Formulario Head </Text>
+          </SafeAreaView>
+
+          <View>
+            <Text>Resultado</Text>
+          </View>
+
+          <View>
+            <Text>Footer</Text>
+          </View>
+     {/* </> */}
+    </Fragment>
   )
 }
+
+
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#f00',
+    height:200,
+    borderBottomLeftRadius:30,
+    borderBottomRightRadius:30,
+    alignItems:"center"
+  }
+})
 
 export default App
